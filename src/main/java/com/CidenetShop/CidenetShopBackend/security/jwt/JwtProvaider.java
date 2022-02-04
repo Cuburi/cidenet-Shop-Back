ae559c038edd119bb09adb867f79a4b5da057bf6
Cuburi <yormanalexis2001@gmail.com>
2022-02-03 23:16:03 -0500

    Actualización del token para acceder todo por medio del payload

--- src/main/java/com/CidenetShop/CidenetShopBackend/security/jwt/JwtProvaider.java
+++ src/main/java/com/CidenetShop/CidenetShopBackend/security/jwt/JwtProvaider.java
@@ -7,10 +7,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import java.text.ParseException;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class JwtProvaider {
@@ -24,21 +27,22 @@ public class JwtProvaider {
 
     public String generateToken(Authentication authentication){
         UserMain userMain = (UserMain) authentication.getPrincipal();
-
+        List<String> roles = userMain.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList());
         return Jwts.builder().setSubject(userMain.getUsername())
+                .claim("roles",roles)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(new Date().getTime()+expiration*1000))
-                .signWith(SignatureAlgorithm.HS512, secret)
+                .signWith(SignatureAlgorithm.HS512, secret.getBytes())
                 .compact();
     }
 
     public String getUserEmailFromToken(String token){
-        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody().getSubject();
+        return Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token).getBody().getSubject();
     }
 
     public boolean validateToken(String token){
         try {
-            Jwts.parser().setSigningKey(secret).parseClaimsJws(token);
+            Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token);
             return true;
         }catch (MalformedJwtException e){
             logger.error("Token mal formado");
